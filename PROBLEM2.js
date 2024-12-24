8d29997cf1712d4a95190af3c3b6f40bb3cc9b73
abhaysanyal59 <abhaysanyal59@gmail.com>
2024-12-24 17:48:08 +0530

    Update PROBLEM2.js

--- PROBLEM2.js
+++ PROBLEM2.js
@@ -1,33 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
 
 const App = () => {
   const [users, setUsers] = useState([]);
 
-  const fetchingUser = async () => {
-    try {
-      const response = await axios.get("https://swapi.dev/api/people/23");
-      setUsers([...users, response.data]);
-    } catch (error) {
-      console.log("Error in Fetching User", error.message);
-    }
-  };
+  useEffect(() => {
+    const fetchingUser = async () => {
+      try {
+        const response = await axios.get("https://swapi.dev/api/people/23");
+        setUsers([...users, response.data]);
+      } catch (error) {
+        console.log("Error in Fetching User", error.message);
+      }
+    };
+  }, []);
+
   const handleDelete = (index) => {
     const updatedUsers = [...users];
     updatedUsers.splice(index, 1);
     setUsers(updatedUsers);
   };
-
   return (
     <div>
       <h1>Random Number</h1>
-      <button onClick={fetchingUser}></button>
+      <button onClick={fetchingUser}>Add User</button>
       <ul>
-        {users.map((index, user) => (
+        {users.map((user, index) => (
           <li key={index}>
-            {user}
-            <button onClick={(index) => handleDelete}>Click to Delete</button>
+            {user.id}
+            <button onClick={() => handleDelete(index)}>Click to Delete</button>
           </li>
         ))}
       </ul>
